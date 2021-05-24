8a5b942ff8138bbf246616dba747a144d6a7ea90
AkaiNezumi <froura@ar-go.co>
2021-05-24 16:59:01 +0200

    [+] master - Changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,8 +5,12 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [ RouterTestingModule ],
-			declarations: [ AppComponent ],
+			imports: [
+				RouterTestingModule,
+			],
+			declarations: [
+				AppComponent,
+			],
 		}).compileComponents();
 	});
 
@@ -26,6 +30,8 @@ describe('AppComponent', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.nativeElement;
-		expect(compiled.querySelector('.content span').textContent).toContain('FogHorn app is running!');
+		expect(compiled.querySelector('.content span').textContent).toContain(
+			'FogHorn app is running!'
+		);
 	});
 });
